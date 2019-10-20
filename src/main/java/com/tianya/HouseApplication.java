900b3a8b00e7a87b77cabe870668fa8aea42234c
wenbochang888 <727987105@qq.com>
2019-10-20 11:49:00 +0800

    test

--- src/main/java/com/tianya/HouseApplication.java
+++ src/main/java/com/tianya/HouseApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HouseApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(HouseApplication.class, args);
 	}
 }
